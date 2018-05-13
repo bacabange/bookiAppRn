58f5f3465c3d416cf0a9ec2f95758d20cac1fa3b
Stiven <stivencastillo.90@gmail.com>
2018-05-13 00:16:41 -0500

    Install packages, create splash screen

---
+++ app/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+import { Splash } from './screens/Splash';
+
+EStyleSheet.build({
+  $primary: '#ef476f',
+});
+
+export default Splash;

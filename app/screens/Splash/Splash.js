60d2a2fc3f41266064929d770777a3333c809ced
Stiven <stivencastillo.90@gmail.com>
2018-05-19 01:19:12 -0500

    Create logo component in splash

--- app/screens/Splash/Splash.js
+++ app/screens/Splash/Splash.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text, StatusBar } from 'react-native';
 
 import { Container } from 'app/components/Container';
 import { Logo } from 'app/components/Logo';
 
-export default () => (
-	<Container>
-		<StatusBar translucent={false} barStyle="light-content" />
-		<Logo />
-	</Container>
-);
+export default class Splash extends Component {
+	componentDidMount() {
+		console.log('Montando');
+	}
+
+	render() {
+		return (
+			<Container>
+				<StatusBar translucent={false} barStyle="light-content" />
+				<Logo />
+			</Container>
+		);
+	}
+}
